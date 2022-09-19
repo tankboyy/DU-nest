7bb9e9aeb6a77a9ac53c1e3b5b334b5772914e67
ksj <tankboya@naver.com>
2022-09-19 17:02:11 +0900

    ?

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameResolver } from './game.resolver';
 import { firebaseConfig } from "../firebase";
-import { LogsModule } from "../logs/logs.module";
 import { UsersModule } from "../users/users.module";
+import { LogsModule } from "../logs/logs.module";
 
 @Module({
   imports: [firebaseConfig, UsersModule, LogsModule],
