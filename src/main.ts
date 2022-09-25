32213c7edd945d25f9230d81c0f93ce627d25379
ksj <tankboya@naver.com>
2022-09-25 15:46:21 +0900

    dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
+dotenv.config({
+  path: path.resolve(
+    (process.env.NODE_ENV === 'production') ? '.env'
+      : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.env.dev'
+  )
+});
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3001);
+  console.log(process.env.CLIENT_URL)
 }
 
 bootstrap();
